a852db9cb3de58155c79e7dc5fc6161d449201f7
Lobo <matheuscardoso1@poli.ufrj.br>
2020-09-23 23:02:16 -0300

    updated main.py to make it more descriptive. Added the first graph of the brute signal to the imgs folder

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print('oi')
+#Deste arquivo, rodaremos sequencialmente todos os outros.
+#Esse processo varrerá desde a aquisição do sinal bruto da instrumentação do reator,
+#até o último plot definitivo.
+
+print('esse será o nosso arquivo principal')
