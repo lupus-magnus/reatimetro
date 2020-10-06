52d0bb22178fcf60bb341b4c8effd8693f442d3e
Lobo <matheuscardoso1@poli.ufrj.br>
2020-10-06 18:26:01 -0300

    modified main code outside jupyter logtolin.py to include the section in which it creates data.csv and adjusted the initial multiplication factor to 10**6 instead of 1

--- logtolin.py
+++ logtolin.py
@@ -1,11 +1,11 @@
 import pandas as pd 
-#O plano é ter três variáveis.
+#O plano e ter tres variaveis.
 #um contador i: encarregado do loop pelas linhas
-#um delta: encarregado de ver a variação
+#um delta: encarregado de ver a variacao
 #entre o sinal atual e o anterior
 #e um multiplicador n: pra se encarregar das
-#mudanças de base quando delta for maior que um valor
-#pré-estipulado.
+#mudancas de base quando delta for maior que um valor
+#pre-estipulado.
 
 df = pd.read_csv('signal.csv')
 
@@ -14,10 +14,10 @@ total_rows = len(df.signal)
 #Inicializando meu contador:
 i = 0
 new_column = ['lin_signal']
-n = 1 #multiplicador
+n = 10**(-6) #multiplicador
 while (i < (total_rows)):
     delta = 0
-    if i > 1: #Só para excluir a exceção que ocorrerá no primeiro valor
+    if i > 1: #So para excluir a excecao que ocorrera no primeiro valor
         delta = df.signal[i] - df.signal[i-1]
     if (delta**2 > 300**2):
         n*=10
@@ -26,7 +26,7 @@ while (i < (total_rows)):
     i+=1
 print(new_column[0:1500])
 
-#A partir daqui vou tentar olhar o gráfico para conferir se está tudo batendo.
+#A partir daqui vou tentar olhar o grafico para conferir se esta tudo batendo.
 import matplotlib.pyplot as plt
 
 #Explicitando o eixo x e y e plotando seus dados.
@@ -38,3 +38,17 @@ plt.title('linearized signal')
 plt.xlabel('time (s)')
 plt.ylabel('linear power signal')
 plt.show()
+
+
+#A partir daqui, estamos nos concentrando em armazenar nossos valores num arquivo chamado data.csv
+df = pd.read_csv('signal.csv')
+df.to_csv('data.csv', index=False)
+df.head()
+
+new_column_pd = pd.Series(new_column[1:])
+
+df2 = pd.read_csv('data.csv')
+df2['lin_signal'] = new_column_pd
+df2.head()
+
+df2.to_csv('data.csv', index=False)
